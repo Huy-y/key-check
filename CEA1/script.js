ad6d1c181a19ec43207dda2a39bedcaacd62591f
Huy Trần <48351013+Huy-y@users.noreply.github.com>
2024-07-24 12:01:11 +0700

    fix: update

--- CEA1/script.js
+++ CEA1/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  fetch("CEAA201.json")
+  fetch("CEA1.json")
       .then((response) => response.json())
       .then((data) => {
         const questionsContainer = document.getElementById("questions");
